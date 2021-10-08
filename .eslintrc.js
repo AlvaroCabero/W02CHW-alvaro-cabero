5de1f5a6052d4071b03a450461cc143acfc52b6c
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-08 18:14:56 +0200

    :arrow_up: Added initial files configuration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jester "
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+  }
+};
